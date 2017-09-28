082fc1ff9fa4dfe5c4364fccd10145fa927bc905
Kartik Kannapur <kartikkannapur@gmail.com>
2017-09-27 22:55:40 -0400

    Algorithmic Toolbox - Week 1 - Complete

---
+++ 02_Coursera_Algorithmic_Toolbox/Week_01_AplusB.py
@@ -0,0 +1,10 @@
+__author__ = "Kartik Kannapur"
+
+import sys
+
+print("--- START ---")
+input = sys.stdin.readline()
+tokens = input.split()
+a = int(tokens[0])
+b = int(tokens[1])
+print(a + b)
---
+++ 02_Coursera_Algorithmic_Toolbox/Week_01_MaximumPairwiseProduct.py
@@ -0,0 +1,32 @@
+__author__ = "Kartik Kannapur"
+
+# #Import Libraries
+import sys
+
+# #Algorithm:
+# #Essentially we need to pick the 2 largest elements from the array
+# #Method 1: Sort the array and select the two largest elements - Very expensive
+# #Method 2: Scan the entire array twice by maintaining two indexes - Max1 and Max 2 - 
+# #Can this be reduced to one operation?
+
+# #Method 3: Scan the array once, keeping a track of the largest element and the second largest
+# #while looping through the array itself.
+
+arr_len = int(sys.stdin.readline())
+arr_vals = sys.stdin.readline()
+arr = [int(elem) for elem in arr_vals.split()]
+
+max_one = arr[0]
+max_two = arr[0]
+
+# #Linear Search 
+for element in arr:
+	if element >= max_one:
+		max_two = max_one
+		max_one = element
+
+	if (element > max_two) and (element < max_one):
+		max_two = element
+
+
+print(max_one, max_two, "Product:", (max_one*max_two))

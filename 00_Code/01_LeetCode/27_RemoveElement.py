585f3f4f807514291167db6b4bf13b9fc0e4d670
Kartik Kannapur <kartikkannapur@gmail.com>
2017-12-28 00:53:41 -0500

    LeetCode - Problem 27 - Solved

---
+++ 00_Code/01_LeetCode/27_RemoveElement.py
@@ -0,0 +1,19 @@
+# #Given an array and a value, remove all instances of that value in-place and return the new length.
+
+class Solution(object):
+    def removeElement(self, nums, val):
+        """
+        :type nums: List[int]
+        :type val: int
+        :rtype: int
+        """
+        # #Method 1
+        my_nums = nums[:]
+        for ele in my_nums:
+            if ele == val:
+                nums.remove(ele)
+
+        # #Method 2
+        while val in nums:
+            nums.remove(val)
+

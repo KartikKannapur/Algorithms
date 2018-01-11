81187112e09cd07f86b97e19938bfc2f69387d77
Kartik Kannapur <kartikkannapur@gmail.com>
2018-01-11 01:55:44 -0500

    LeetCode - Problem 347 - Solved

--- 00_Code/01_LeetCode/347_TopKFrequentElements.py
+++ 00_Code/01_LeetCode/347_TopKFrequentElements.py
@@ -3,6 +3,8 @@ Given a non-empty array of integers, return the k most frequent elements.
 
 For example,
 Given [1,1,1,2,2,3] and k = 2, return [1,2].
+
+Your runtime beats 52.16 % of python submissions.
 """
 
 

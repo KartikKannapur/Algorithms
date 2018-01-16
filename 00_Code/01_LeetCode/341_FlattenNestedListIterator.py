52459793117ad1b10a8edbf0a6cb72b0e9a23f9b
Kartik Kannapur <kartikkannapur@gmail.com>
2018-01-15 19:33:37 -0500

    LeetCode - Problems 341, 385 - Solved

---
+++ 00_Code/01_LeetCode/341_FlattenNestedListIterator.py
@@ -0,0 +1,81 @@
+"""
+Given a nested list of integers, implement an iterator to flatten it.
+
+Each element is either an integer, or a list -- whose elements may also be integers or other lists.
+
+Example 1:
+Given the list [[1,1],2,[1,1]],
+
+By calling next repeatedly until hasNext returns false, the order of elements returned by next should be: [1,1,2,1,1].
+
+Example 2:
+Given the list [1,[4,[6]]],
+
+By calling next repeatedly until hasNext returns false, the order of elements returned by next should be: [1,4,6].
+
+Your runtime beats 83.76 % of python submissions.
+"""
+
+
+# """
+# This is the interface that allows for creating nested lists.
+# You should not implement it, or speculate about its implementation
+# """
+# class NestedInteger(object):
+#    def isInteger(self):
+#        """
+#        @return True if this NestedInteger holds a single integer, rather than a nested list.
+#        :rtype bool
+#        """
+#
+#    def getInteger(self):
+#        """9
+#        @return the single integer that this NestedInteger holds, if it holds a single integer
+#        Return None if this NestedInteger holds a nested list
+#        :rtype int
+#        """
+#
+#    def getList(self):
+#        """
+#        @return the nested list that this NestedInteger holds, if it holds a nested list
+#        Return None if this NestedInteger holds a single integer
+#        :rtype List[NestedInteger]
+#        """
+
+class NestedIterator(object):
+    def __init__(self, nestedList):
+        """
+        Initialize your data structure here.
+        :type nestedList: List[NestedInteger]
+        """
+
+        def gen(nestedList):
+            for x in nestedList:
+                if x.isInteger():
+                    yield x.getInteger()
+                else:
+                    for y in gen(x.getList()):
+                        yield y
+
+        self.gen = gen(nestedList)
+
+    def next(self):
+        """
+        :rtype: int
+        """
+        return self.value
+
+    def hasNext(self):
+        """
+        :rtype: bool
+        """
+        try:
+            self.value = next(self.gen)
+            return True
+        except StopIteration:
+            return False
+
+
+# Your NestedIterator object will be instantiated and called as such:
+# i, v = NestedIterator(nestedList), []
+# while i.hasNext(): v.append(i.next())

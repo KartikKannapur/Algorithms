873854c9b34befb1ee6dbd43cc556faa51596f15
Kartik Kannapur <kartikkannapur@gmail.com>
2018-01-18 20:06:23 -0500

    LeetCode - Problems 242, 349 - Solved

---
+++ 00_Code/01_LeetCode/242_ValidAnagram.py
@@ -0,0 +1,24 @@
+"""
+Given two strings s and t, write a function to determine if t is an anagram of s.
+
+For example,
+s = "anagram", t = "nagaram", return true.
+s = "rat", t = "car", return false.
+
+Note:
+You may assume the string contains only lowercase alphabets.
+
+Follow up:
+What if the inputs contain unicode characters? How would you adapt your solution to such case?
+Your runtime beats 47.05 % of python submissions.
+"""
+
+
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        return sorted(s) == sorted(t)

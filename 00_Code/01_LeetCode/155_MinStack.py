e703d5405420b070f470ce2e49c04a921dda6e5c
Kartik Kannapur <kartikkannapur@gmail.com>
2018-01-22 16:08:27 -0500

    LeetCode - Problems 58, 155, 627 - Solved

--- 00_Code/01_LeetCode/155_MinStack.py
+++ 00_Code/01_LeetCode/155_MinStack.py
@@ -6,11 +6,20 @@ pop() -- Removes the element on top of the stack.
 top() -- Get the top element.
 getMin() -- Retrieve the minimum element in the stack.
 
-Your runtime beats 15.18 % of python submissions.
+Your runtime beats 64.02 % of python submissions.
 """
 
 
 class MinStack(object):
+    """
+    Updated Algo got me from 15.18 %  to 64.02 %
+    Algo:
+    Rather than implementing a naive version of the stack,
+    store each element as a tuple(element, min_element_in_stack)
+
+    Your runtime beats 64.02 % of python submissions.
+    """
+
     def __init__(self):
         """
         initialize your data structure here.
@@ -22,31 +31,40 @@ class MinStack(object):
         :type x: int
         :rtype: void
         """
-        self.stack.append(x)
+        if self.stack:
+            self.stack.append((x, min(x, self.stack[-1][1])))
+        else:
+            self.stack.append((x, x))
 
     def pop(self):
         """
         :rtype: void
         """
-        self.stack.pop()
+        if self.stack:
+            self.stack.pop()
 
     def top(self):
         """
         :rtype: int
         """
-        return self.stack[-1]
+        if self.stack:
+            return self.stack[-1][0]
+        return None
 
     def getMin(self):
         """
         :rtype: int
         """
-        return min(self.stack)
+        if self.stack:
+            return self.stack[-1][1]
+        else:
+            return None
 
 
 
-        # Your MinStack object will be instantiated and called as such:
-        # obj = MinStack()
-        # obj.push(x)
-        # obj.pop()
-        # param_3 = obj.top()
-        # param_4 = obj.getMin()
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(x)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()        # param_4 = obj.getMin()
--- 00_Code/01_LeetCode/58_LengthofLastWord.py
+++ 00_Code/01_LeetCode/58_LengthofLastWord.py
@@ -1,14 +1,19 @@
-# #Given a string s consists of upper/lower-case alphabets and
-# #empty space characters ' ', return the length of last word in the string.
-# #If the last word does not exist, return 0.
-# #Note: A word is defined as a character sequence consists of non-space
-# #characters only.
-# #Your runtime beats 47.33 % of python submissions.
-
+"""
+Given a string s consists of upper/lower-case alphabets and
+empty space characters ' ', return the length of last word in the string.
+If the last word does not exist, return 0.
+Note: A word is defined as a character sequence consists of non-space
+characters only.
+Your runtime beats 75.33 % of python submissions.
+"""
 class Solution(object):
     def lengthOfLastWord(self, s):
         """
         :type s: str
         :rtype: int
+        """
+        """
+        Method 1
+        Your runtime beats 75.33 % of python submissions.
         """
         return len(s.rstrip(' ').split(' ')[-1])

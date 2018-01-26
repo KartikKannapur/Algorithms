5bad5dc1cf3ddbe1f2334020fbd75517702d6913
Kartik Kannapur <kartikkannapur@gmail.com>
2018-01-26 18:15:39 -0500

    HackerRank- SQL - Problem 37 - Solved

---
+++ 00_Code/02_HackerRank/SQL/37_WeatherObservationStation18.sql
@@ -0,0 +1,11 @@
+/*
+Consider  and  to be two points on a 2D plane.
+
+a happens to equal the minimum value in Northern Latitude (LAT_N in STATION).
+b happens to equal the minimum value in Western Longitude (LONG_W in STATION).
+c happens to equal the maximum value in Northern Latitude (LAT_N in STATION).
+d happens to equal the maximum value in Western Longitude (LONG_W in STATION).
+Query the Manhattan Distance between points  and  and round it to a scale of  decimal places.
+ */
+
+SELECT ROUND(((MAX(LAT_N) - MIN(LAT_N)) + (MAX(LONG_W) - MIN(LONG_W))), 4) FROM STATION;

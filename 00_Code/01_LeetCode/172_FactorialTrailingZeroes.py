edb3412796ef88f066f0c475e0ffb151f58277c4
Kartik Kannapur <kartikkannapur@gmail.com>
2018-01-27 15:34:01 -0500

    LeetCode - Problems 172, 728 - Solved

---
+++ 00_Code/01_LeetCode/172_FactorialTrailingZeroes.py
@@ -0,0 +1,25 @@
+"""
+Given an integer n, return the number of trailing zeroes in n!.
+
+Note: Your solution should be in logarithmic time complexity.
+"""
+
+
+class Solution(object):
+    def trailingZeroes(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        """
+        Method 1:
+        n/5 + n/25 + n/625 ...
+
+        Your runtime beats 40.59 % of python submissions.
+        """
+        counter = 0;
+
+        while n:
+            n = n // 5
+            counter += n
+        return counter

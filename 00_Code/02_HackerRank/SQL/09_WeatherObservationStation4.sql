78da0205bf8cbb94738a57e2428323a76c567dd4
Kartik Kannapur <kartikkannapur@gmail.com>
2018-01-29 21:42:02 -0500

    HackerRank- SQL - Problems 9, 16, 35, 43 - Solved

--- 00_Code/02_HackerRank/SQL/09_WeatherObservationStation4.sql
+++ 00_Code/02_HackerRank/SQL/09_WeatherObservationStation4.sql
@@ -1,1 +1,6 @@
+/*
+Let A be the number of CITY entries in STATION, and let B be the number of distinct CITY names in STATION; query the value of A - B from STATION. In other words, find the difference between the total number of CITY entries in the table and the number of distinct CITY entries in the table.
+ */
+
+
 SELECT COUNT(CITY) - COUNT(DISTINCT CITY) FROM STATION;

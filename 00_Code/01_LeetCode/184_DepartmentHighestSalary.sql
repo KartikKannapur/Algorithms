7a2ca15f4bb4390a21359a37143b292de1521d09
Kartik Kannapur <kartikkannapur@gmail.com>
2018-01-31 14:20:57 -0500

    LeetCode - Problems 177, 184, 187 - Solved

---
+++ 00_Code/01_LeetCode/184_DepartmentHighestSalary.sql
@@ -0,0 +1,36 @@
+/*
+The Employee table holds all employees. Every employee has an Id, a salary, and there is also a column for the department Id.
+
++----+-------+--------+--------------+
+| Id | Name  | Salary | DepartmentId |
++----+-------+--------+--------------+
+| 1  | Joe   | 70000  | 1            |
+| 2  | Henry | 80000  | 2            |
+| 3  | Sam   | 60000  | 2            |
+| 4  | Max   | 90000  | 1            |
++----+-------+--------+--------------+
+The Department table holds all departments of the company.
+
++----+----------+
+| Id | Name     |
++----+----------+
+| 1  | IT       |
+| 2  | Sales    |
++----+----------+
+Write a SQL query to find employees who have the highest salary in each of the departments. For the above tables, Max has the highest salary in the IT department and Henry has the highest salary in the Sales department.
+
++------------+----------+--------+
+| Department | Employee | Salary |
++------------+----------+--------+
+| IT         | Max      | 90000  |
+| Sales      | Henry    | 80000  |
++------------+----------+--------+
+*/
+
+SELECT B.Department, A.Name AS "Employee", A.Salary FROM Employee A JOIN
+(SELECT D.Name AS "Department", MAX(E.Salary) AS "Salary", D.Id AS "Id" FROM
+Employee E JOIN Department D ON
+E.DepartmentId = D.Id
+GROUP BY E.DepartmentId) B
+ON A.Salary = B.Salary AND A.DepartmentId = B.Id;
+
---
+++ 00_Code/01_LeetCode/187_RepeatedDNASequences.py
@@ -0,0 +1,30 @@
+"""
+All DNA is composed of a series of nucleotides abbreviated as A, C, G, and T, for example: "ACGAATTCCG". When studying DNA, it is sometimes useful to identify repeated sequences within the DNA.
+
+Write a function to find all the 10-letter-long sequences (substrings) that occur more than once in a DNA molecule.
+
+For example,
+
+Given s = "AAAAACCCCCAAAAACCCCCCAAAAAGGGTTT",
+
+Return:
+["AAAAACCCCC", "CCCCCAAAAA"].
+"""
+
+class Solution(object):
+    def findRepeatedDnaSequences(self, s):
+        """
+        :type s: str
+        :rtype: List[str]
+        """
+        """
+        Method 1: Brute Force + Dictionary
+        Your runtime beats 46.08 % of python submissions.
+        """
+        if not s or len(s) < 10:
+            return []
+
+        d = {}
+        for i in range(len(s) - 9):
+            d[s[i:i + 10]] = d.get(s[i:i + 10], 0) + 1
+        return [res for res in d.keys() if d[res] > 1]

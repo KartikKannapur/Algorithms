83593ce8a76af011bc2598987df25651beb7a792
Kartik Kannapur <kartikkannapur@gmail.com>
2018-02-11 18:46:03 -0500

    ML Topics - Updated

--- 00_Code/01_LeetCode/000_SelectRandomMLTopics.py
+++ 00_Code/01_LeetCode/000_SelectRandomMLTopics.py
@@ -1,5 +1,13 @@
 import random
 
-arr_ml_topics = ["SVM - Intro to SVC", "SVM - SVC and Kernels", "Naive Bayes"]
+arr_ml_topics = [
+    "Bias Variance Tradeoff",
+    "Distance Metrics",
+    "kNN",
+    "Naive Bayes",
+    "ROC Curve - Binary Classification"
+    "SVM - Intro to SVC",
+    "SVM - SVC and Kernels"
+]
 
 print(random.choices(arr_ml_topics, k=2))

f783d3a3c4e3d560a71bb137c56d974dad78b4dc
Kartik Kannapur <kartikkannapur@gmail.com>
2018-02-13 11:36:06 -0500

    LeetCode - Problems 17, 75, 150 - Solved

--- 00_Code/01_LeetCode/150_EvaluateReversePolishNotation.py
+++ 00_Code/01_LeetCode/150_EvaluateReversePolishNotation.py
@@ -12,62 +12,70 @@ Your runtime beats 51.48 % of python submissions.
 
 
 class Solution(object):
+    #     """
+    #     Method 1: Stack
+    #     eval()
+    #     Your runtime beats 6.71 % of python submissions.
+    #     """
+    #     def evalRPN(self, tokens):
+    #         """
+    #         :type tokens: List[str]
+    #         :rtype: int
+    #         """
+
+    #         s = []
+
+    #         for ele in tokens:
+    #             if ele in ["+", "-", "*", "/"]:
+    #                 temp1 = s.pop()
+    #                 temp2 = s.pop()
+
+    #                 if ele == "/":
+    #                     ele = "//"
+    #                     new_temp = int(float(temp2)/float(temp1))
+    #                 else:
+    #                     new_temp = eval("(" + temp2 + ele + temp1 + ")")
+
+
+    #                 s.append(str(new_temp))
+    #             else:
+    #                 s.append(ele)
+
+    #         return (int(s[0]))
+
+    """
+    Method 2: Stack without eval()
+    Create a dict with +-*/ operations
+
+    Your runtime beats 87.57 % of python submissions.
+    """
+
+    def __init__(self):
+        self.dict_ops = {
+            '+': lambda x, y: x + y,
+            '-': lambda x, y: x - y,
+            '*': lambda x, y: x * y,
+            '/': lambda x, y: int(float(x) / float(y))
+        }
+
     def evalRPN(self, tokens):
         """
         :type tokens: List[str]
         :rtype: int
         """
-        """
-        Method 1: Stack
-        eval()
-        Your runtime beats 6.71 % of python submissions.
-        """
-        #         s = []
-
-        #         for ele in tokens:
-        #             if ele in ["+", "-", "*", "/"]:
-        #                 temp1 = s.pop()
-        #                 temp2 = s.pop()
-
-        #                 if ele == "/":
-        #                     ele = "//"
-        #                     new_temp = int(float(temp2)/float(temp1))
-        #                 else:
-        #                     new_temp = eval("(" + temp2 + ele + temp1 + ")")
-
-
-        #                 s.append(str(new_temp))
-        #             else:
-        #                 s.append(ele)
-
-        #         return (int(s[0]))
-
-        """
-        Method 2: Stack without eval()
-        Create a dict with +-*/ operations        
-
-        Your runtime beats 51.48 % of python submissions.
-        """
-        dict_ops = {
-            '+': lambda x, y: int(float(x) + float(y)),
-            '-': lambda x, y: int(float(x) - float(y)),
-            '*': lambda x, y: int(float(x) * float(y)),
-            '/': lambda x, y: int(float(x) / float(y))
-        }
+        if not tokens:
+            return 0
 
         s = []
 
         for ele in tokens:
-            if ele in ['+', '-', '*', '/']:
+            if ele in self.dict_ops:
                 temp1 = s.pop()
                 temp2 = s.pop()
 
-                s.append(dict_ops[ele](temp2, temp1))
+                s.append(self.dict_ops[ele](temp2, temp1))
             else:
                 s.append(int(ele))
         return s[0]
-
-
-
 
 
--- 00_Code/01_LeetCode/17_LetterCombinationsofaPhoneNumber.py
+++ 00_Code/01_LeetCode/17_LetterCombinationsofaPhoneNumber.py
@@ -6,7 +6,6 @@ dict_numbers = {"2" : list("abc"), "3" : list("def"),
                 "6" : list("mno"), "7" : list("pqrs"),
                 "8" : list("tuv"), "9" : list("wxyz")}
 
-Your runtime beats 32.82 % of python submissions.
 """
 
 
@@ -16,7 +15,12 @@ class Solution(object):
         :type digits: str
         :rtype: List[str]
         """
-        # #Method 1
+        """
+        Method 1
+        Your runtime beats 58.77 % of python submissions.
+        Among all the other solutions - Iterative, Backtracking; this method
+        seemed to yield the best accuracy.
+        """
         from itertools import product
 
         dict_numbers = {"2": list("abc"), "3": list("def"),
@@ -34,4 +38,3 @@ class Solution(object):
             prod = ["".join(i) for i in product(prod, dict_numbers[digits[k]])]
 
         return (prod)
-

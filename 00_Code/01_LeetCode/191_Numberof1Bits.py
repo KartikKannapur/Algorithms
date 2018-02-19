78e16e79cba78d7981f8b03c90708037e6e84fb4
Kartik Kannapur <kartikkannapur@gmail.com>
2018-02-19 18:54:44 -0500

    LeetCode - Problems 191, 231 - Solved

--- 00_Code/01_LeetCode/191_Numberof1Bits.py
+++ 00_Code/01_LeetCode/191_Numberof1Bits.py
@@ -1,8 +1,8 @@
-# #Write a function that takes an unsigned integer and returns
-# #the number of ’1' bits it has (also known as the Hamming weight).
-# #For example, the 32-bit integer ’11' has binary representation
-# #00000000000000000000000000001011, so the function should return 3.
-# #Your runtime beats 29.11 % of python submissions.
+"""
+Write a function that takes an unsigned integer and returns the number of ’1' bits it has (also known as the Hamming weight).
+
+For example, the 32-bit integer ’11' has binary representation 00000000000000000000000000001011, so the function should return 3.
+"""
 
 class Solution(object):
     def hammingWeight(self, n):
@@ -10,4 +10,20 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        return bin(n).count("1")
+        """
+        Method 1: Stings
+        Your runtime beats 45.97 % of python submissions.
+        """
+        # return bin(n).count("1")
+
+
+        """
+        Method 2: Brian Kernighan’s Algorithm
+        Your runtime beats 75.00 % of python submissions.
+        """
+        counter = 0
+        while n:
+            n = n & (n - 1)
+            counter += 1
+
+        return counter
--- 00_Code/01_LeetCode/231_PowerofTwo.py
+++ 00_Code/01_LeetCode/231_PowerofTwo.py
@@ -12,19 +12,20 @@ class Solution(object):
         """
         Method 1: Repeated Division
         """
-        while n % 2 == 0:
-            n = n // 2
+        #         while n % 2 == 0:
+        #             n = n // 2
 
-        if n == 1: return True
-        else: return False
+        #         if n == 1: return True
+        #         else: return False
 
 
         """
         Method 2: !(n&(n-1))
         Your runtime beats 32.94 % of python submissions.
         """
-        if n > 0:
-            return not (n & (n - 1))
-        else:
-            return False
+        # if n > 0: return not(n&(n - 1))
+        # else: return False
+
+        # #Golfing
+        return n > 0 and not (n & (n - 1))
 

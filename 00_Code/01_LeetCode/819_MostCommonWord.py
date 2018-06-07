949c432fe8a1843feffba4dfe0063a96d33db5a7
Kartik Kannapur <kartikkannapur@gmail.com>
2018-06-07 19:35:39 -0400

    LeetCode - Problems 819 - Solved

---
+++ 00_Code/01_LeetCode/819_MostCommonWord.py
@@ -0,0 +1,50 @@
+"""
+Given a paragraph and a list of banned words, return the most frequent word that is not in the list of banned words.  It is guaranteed there is at least one word that isn't banned, and that the answer is unique.
+
+Words in the list of banned words are given in lowercase, and free of punctuation.  Words in the paragraph are not case sensitive.  The answer is in lowercase.
+
+Example:
+Input:
+paragraph = "Bob hit a ball, the hit BALL flew far after it was hit."
+banned = ["hit"]
+Output: "ball"
+Explanation:
+"hit" occurs 3 times, but it is a banned word.
+"ball" occurs twice (and no other word does), so it is the most frequent non-banned word in the paragraph.
+Note that words in the paragraph are not case sensitive,
+that punctuation is ignored (even if adjacent to words, such as "ball,"),
+and that "hit" isn't the answer even though it occurs more because it is banned.
+
+"""
+
+
+class Solution(object):
+    def mostCommonWord(self, paragraph, banned):
+        """
+        :type paragraph: str
+        :type banned: List[str]
+        :rtype: str
+        """
+
+        # #Method 1:
+        # 46 / 46 test cases passed.
+        # Status: Accepted
+        # Runtime: 46 ms
+
+        import re
+        d = {}
+        # #Create a counter-dictionary
+        paragraph = re.sub('[^a-zA-Z]+', ' ', paragraph)
+
+        for word in paragraph.lower().split():
+            if word in d:
+                d[word] += 1
+            else:
+                d[word] = 1
+        print(d)
+        # #sort by value
+        for ele in sorted([(value, key) for (key, value) in d.items()], reverse=True):
+            if ele[1] not in banned:
+                return ele[1]
+
+        return -1

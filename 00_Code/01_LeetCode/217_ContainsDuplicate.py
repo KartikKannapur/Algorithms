55b2439b74800671794868740dea69f95a21e1f5
Kartik Kannapur <kartikkannapur@gmail.com>
2018-07-26 23:04:50 -0400

    LeetCode - Problems 192, 217 - Solved

--- 00_Code/01_LeetCode/217_ContainsDuplicate.py
+++ 00_Code/01_LeetCode/217_ContainsDuplicate.py
@@ -1,25 +1,46 @@
-# #Given an array of integers, find if the array contains any duplicates.
-# #Your function should return true if any value appears at least twice
-# #in the array, and it should return false if every element is distinct.
-# #Your runtime beats 54.14 % of python submissions.
+"""
+Given an array of integers, find if the array contains any duplicates.
 
-class Solution(object):
+Your function should return true if any value appears at least twice in the array, and it should return false if every element is distinct.
+
+Example 1:
+
+Input: [1,2,3,1]
+Output: true
+Example 2:
+
+Input: [1,2,3,4]
+Output: false
+Example 3:
+
+Input: [1,1,1,3,3,4,3,2,4,2]
+Output: true
+
+
+"""
+
+
+class Solution:
     def containsDuplicate(self, nums):
         """
         :type nums: List[int]
         :rtype: bool
         """
-        # #Method 1:
-        if nums:
-            nums.sort()
-            return len(nums) != len(list(set(nums)))
-        return False
 
-        # #Method 2:
-        if nums:
-            nums.sort()
+        """
+        Method 1:
+        """
+        #         if nums:
+        #             nums.sort()
 
-            for i in range(1, len(nums)):
-                if nums[i] == nums[i - 1]:
-                    return True
-        return False
+        #             for i in range(1,len(nums)):
+        #                 if nums[i] == nums[i-1]:
+        #                     return True
+        #         return False
+
+
+        """
+        Method 2:
+        Your runtime beats 99.78 % of python3 submissions.
+        """
+        return len(nums) != len(set(nums))

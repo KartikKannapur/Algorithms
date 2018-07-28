39bc560d2946e15cf876e2333b866c6225138764
Kartik Kannapur <kartikkannapur@gmail.com>
2018-07-27 21:58:22 -0400

    LeetCode - Problems 720 - Solved

--- 00_Code/01_LeetCode/720_LongestWordinDictionary.py
+++ 00_Code/01_LeetCode/720_LongestWordinDictionary.py
@@ -1,29 +1,48 @@
+"""
+Given a list of strings words representing an English Dictionary, find the longest word in words that can be built one character at a time by other words in words. If there is more than one possible answer, return the longest word with the smallest lexicographical order.
+
+If there is no answer, return the empty string.
+Example 1:
+Input:
+words = ["w","wo","wor","worl", "world"]
+Output: "world"
+Explanation:
+The word "world" can be built one character at a time by "w", "wo", "wor", and "worl".
+Example 2:
+Input:
 words = ["a", "banana", "app", "appl", "ap", "apply", "apple"]
+Output: "apple"
+Explanation:
+Both "apply" and "apple" can be built from other words in the dictionary. However, "apple" is lexicographically smaller than "apply".
+Note:
+
+All the strings in the input will only contain lowercase letters.
+The length of words will be in the range [1, 1000].
+The length of words[i] will be in the range [1, 30].
+
+"""
 
 
-arr = []
+class Solution:
+	def longestWord(self, words):
+		"""
+        :type words: List[str]
+        :rtype: str
+        """
 
-for word in words:
-	var_counter = 0
-	for counter in range(len(word)):
-		if word[:counter] in words:
-			# print(word, word[:counter])
-			var_counter += 1
+		"""
+        Method 1:
+        Your runtime beats 90.58 % of python3 submissions.
+        """
+		word_set = set([''])
+		word_longest = ''
 
-		if len(word) == var_counter+1:
-			# print(word)
-			arr.append(word)
+		words.sort()
+		for ele in words:
+			if ele[:-1] in word_set:
+				word_set.add(ele)
 
+				if len(ele) > len(word_longest):
+					word_longest = ele
 
-l = len(sorted(arr)[-1])
-
-arr2 = []
-for ele in arr:
-	if len(ele) == l:
-		arr2.append(ele)
-
-return(sorted(arr2)[0])
-
-
-	# if all(True if word[:counter] in words else False for counter in range(len(word)+1)) == True:
-	# 	print(word)
+		return word_longest

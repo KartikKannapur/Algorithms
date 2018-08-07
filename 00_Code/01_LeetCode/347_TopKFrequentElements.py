c9783d6a0594811913a7c1b1ad7dcc4bcd25069b
Kartik Kannapur <kartikkannapur@gmail.com>
2018-08-06 23:07:37 -0400

    LeetCode - Problems 347, 692 - Solved

--- 00_Code/01_LeetCode/347_TopKFrequentElements.py
+++ 00_Code/01_LeetCode/347_TopKFrequentElements.py
@@ -8,14 +8,33 @@ Your runtime beats 52.16 % of python submissions.
 """
 
 
-class Solution(object):
+class Solution:
     def topKFrequent(self, nums, k):
         """
         :type nums: List[int]
         :type k: int
         :rtype: List[int]
         """
-        # from collections import Counter
-        # return [ele[0] for ele in Counter(nums).most_common(k)]
 
-        return [item[0] for item in collections.Counter(nums).most_common(k)]
+        """
+        Method 1: Counter Based Method
+
+        Your runtime beats 100.00 % of python3 submissions
+        """
+        # return [item[0] for item in collections.Counter(nums).most_common(k)]
+
+        """
+        Method 2: Using a hash map without Counter
+
+        * Create a hash map with {element : count}
+        * Sort the hash map in reverse order and select first k elements
+
+        Your runtime beats 68.42 % of python3 submissions.
+        """
+        d = {}
+        for ele in nums:
+            if ele in d:
+                d[ele] += 1
+            else:
+                d[ele] = 1
+        return [ele[0] for ele in sorted(d.items(), key=lambda x: x[1], reverse=True)][:k]

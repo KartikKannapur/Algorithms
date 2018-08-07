e9fdea2757b3cf8c5e102badfa0f54164bcb4cf3
Kartik Kannapur <kartikkannapur@gmail.com>
2018-08-06 20:36:14 -0400

    LeetCode - Problems 586, 657 - Solved

--- 00_Code/01_LeetCode/657_JudgeRouteCircle.py
+++ 00_Code/01_LeetCode/657_JudgeRouteCircle.py
@@ -17,15 +17,36 @@ Your runtime beats 77.72 % of python submissions.
 """
 
 
-class Solution(object):
+class Solution:
     def judgeCircle(self, moves):
         """
         :type moves: str
         :rtype: bool
         """
-        # #Method 1:
-        map_path = {"U" : 1, "D": -1, "R": 1,  "L": -1,}
-        return sum([map_path[var_move] for var_move in moves]) == 0
 
-        # #Method 2:
+        """
+        Method 1:
+
+        * The robot moves back to its initial position
+        IF count of left moves == count of right moves
+        IF count of up moves == count of down moves
+
+        * We could assign a cost to each of these operations
+        say +1/-1 scheme and compute the cost of traversal
+        map_path = {"U" : 1, "D": -1, "R": 1,  "L": -1,}
+
+        Your runtime beats 73.96 % of python3 submissions.
+        """
+
+        # map_path = {"U" : 1, "D": -1, "R": 1,  "L": -1,}
+        # return sum([map_path[var_move] for var_move in moves]) == 0
+
+
+        """
+        Method 2:
+        * Cost computation is done via a count operation
+        in a single line
+
+        Your runtime beats 82.29 % of python3 submissions.
+        """
         return moves.count('L') == moves.count('R') and moves.count('U') == moves.count('D')

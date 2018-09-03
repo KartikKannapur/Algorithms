d840422f56b907fa7f5a4d3c2cb3b13cb7d66285
Kartik Kannapur <kartikkannapur@gmail.com>
2018-09-03 10:55:51 -0400

    LeetCode - Problems 2, 13, 263 - Solved

--- 00_Code/01_LeetCode/02_AddTwoNumbers.py
+++ 00_Code/01_LeetCode/02_AddTwoNumbers.py
@@ -3,21 +3,21 @@ You are given two non-empty linked lists representing two non-negative integers.
 
 You may assume the two numbers do not contain any leading zero, except the number 0 itself.
 
-Example
+Example:
 
 Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
 Output: 7 -> 0 -> 8
 Explanation: 342 + 465 = 807.
+
 """
 
 # Definition for singly-linked list.
-# class ListNode(object):
+# class ListNode:
 #     def __init__(self, x):
 #         self.val = x
 #         self.next = None
 
-
-class Solution(object):
+class Solution:
     def addTwoNumbers(self, l1, l2):
         """
         :type l1: ListNode
@@ -26,15 +26,16 @@ class Solution(object):
         """
 
         """
-        Method 1:
-        Your runtime beats 86.03 % of python submissions.
+        Method 1: Using divmod()
+
+        Your runtime beats 54.97 % of python submissions.
         """
 
         carry = 0
         dummy = new_list = ListNode(0)
 
         while l1 or l2 or carry:
-            value1 = value2 = 0
+            value1 = value2 = 0  # #To make sure there is no spill over
             if l1:
                 value1 = l1.val
                 l1 = l1.next
@@ -48,4 +49,3 @@ class Solution(object):
             new_list.next = ListNode(value_res)
             new_list = new_list.next
         return dummy.next
-
--- 00_Code/01_LeetCode/263_UglyNumber.py
+++ 00_Code/01_LeetCode/263_UglyNumber.py
@@ -1,17 +1,49 @@
-# #Write a program to check whether a given number is an ugly number.
-# #Ugly numbers are positive numbers whose prime factors only include
-# #2, 3, 5. For example, 6, 8 are ugly while 14 is not ugly since
-# #it includes another prime factor 7.
-# #Note that 1 is typically treated as an ugly number.
-# #Your runtime beats 52.60 % of python submissions.
+"""
+Write a program to check whether a given number is an ugly number.
 
-class Solution(object):
+Ugly numbers are positive numbers whose prime factors only include 2, 3, 5.
+
+Example 1:
+
+Input: 6
+Output: true
+Explanation: 6 = 2 × 3
+Example 2:
+
+Input: 8
+Output: true
+Explanation: 8 = 2 × 2 × 2
+Example 3:
+
+Input: 14
+Output: false
+Explanation: 14 is not ugly since it includes another prime factor 7.
+
+"""
+
+
+class Solution:
     def isUgly(self, num):
         """
         :type num: int
         :rtype: bool
         """
-        for p in 2, 3, 5:
-            while num % p == 0 < num:
-                num /= p
+
+        """
+        Method 1: Standard Division
+
+        * For each of the numbers - 2, 3, 5
+        keep dividing num by 2,3 or 5, as long
+        as the remainder == 0
+
+        Your runtime beats 100.00 % of python3 submissions.
+        """
+        # #Code block to handle edge-cases
+        if num == 0:
+            return False
+
+        for ele in 2, 3, 5:
+            while num % ele == 0:
+                num = num / ele
+
         return num == 1

361a83224f30670f7c4d7f6d7b1eda801c9324db
Kartik Kannapur <kartikkannapur@gmail.com>
2018-11-04 16:52:53 -0500

    LeetCode - Problems 8, 9, 12 - Solved

--- 00_Code/01_LeetCode/9_PalindromeNumber.py
+++ 00_Code/01_LeetCode/9_PalindromeNumber.py
@@ -1,6 +1,10 @@
 """
 Determine whether an integer is a palindrome. Do this without extra space.
 """
+
+
+# #Determine whether an integer is a palindrome. Do this without extra space.
+
 class Solution(object):
     """
     Method 1:
@@ -10,33 +14,32 @@ class Solution(object):
     Method 2: Two Pointer
     Your runtime beats 21.98 % of python submissions.
     """
-
-    """
-    Function to Reverse a Number:
-
-    result = 0
-    while s:
-        rem = s%10
-        s = s//10
-        
-        result = result*10 + rem
-    
-        print(rem, s, result)
-    """
-
     """
     Method 3:
     Your runtime beats 43.28 % of python submissions.
+
     """
+
+    def reverseInteger(self, num):
+        res = 0
+        while num:
+            carry = num % 10
+            res = res * 10 + carry
+            num = num // 10
+
+        return res
 
     def isPalindrome(self, x):
         """
         :type x: int
         :rtype: bool
         """
+
         if str(x)[0] == "-":
             return False
 
-        if str(x) == str(x)[::-1]:
+        # #if str(x) == str(x)[::-1]:
+        if x == self.reverseInteger(x):
             return True
         return False
+

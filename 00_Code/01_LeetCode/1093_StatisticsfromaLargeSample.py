14f6bd150d3578354d9922fe1d2eccece01fd9e4
Kartik Kannapur <kartikkannapur@gmail.com>
2019-06-23 10:25:48 -0400

    Create 1093_StatisticsfromaLargeSample.py

---
+++ 00_Code/01_LeetCode/1093_StatisticsfromaLargeSample.py
@@ -0,0 +1,69 @@
+"""
+We sampled integers between 0 and 255, and stored the results in an array count:  count[k] is the number of integers we sampled equal to k.
+
+Return the minimum, maximum, mean, median, and mode of the sample respectively, as an array of floating point numbers.  The mode is guaranteed to be unique.
+
+(Recall that the median of a sample is:
+
+The middle element, if the elements of the sample were sorted and the number of elements is odd;
+The average of the middle two elements, if the elements of the sample were sorted and the number of elements is even.)
+ 
+
+Example 1:
+
+Input: count = [0,1,3,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+Output: [1.00000,3.00000,2.37500,2.50000,3.00000]
+Example 2:
+
+Input: count = [0,4,3,2,2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+Output: [1.00000,4.00000,2.18182,2.00000,1.00000]
+"""
+
+class Solution:
+    def sampleStats(self, count: List[int]) -> List[float]:
+        
+        """
+        46 / 46 test cases passed.
+        Status: Accepted
+        Runtime: 36 ms
+        Memory Usage: 13.2 MB
+        """
+        
+        res_min = -1
+        res_max = 0
+        
+        temp_sum = 0
+        temp_non_zero_counter = 0
+        
+        # #Min, Max and Mean Computation
+        for index, value in enumerate(count):
+            if value !=0 and not index:
+                res_min = index
+            elif value != 0 and res_min == -1:
+                # #Min Computation
+                res_min = index
+            elif value != 0:
+                # #Max Computation
+                res_max = index
+                
+            temp_sum += index*value
+            temp_non_zero_counter += value
+
+        
+        # #Mode Computation
+        res_mode = count.index(max(count))
+                
+           
+        # #Median Computation
+        n = sum(count)
+        for i in range(255):
+            count[i + 1] += count[i]
+        median1 = bisect.bisect(count, (n - 1) / 2)
+        median2 = bisect.bisect(count, n / 2)
+        res_median = (median1 + median2) / 2.0
+            
+        return [float(res_min), float(res_max), float(temp_sum/temp_non_zero_counter), float(res_median), float(res_mode)]
+                
+                
+        
+        
